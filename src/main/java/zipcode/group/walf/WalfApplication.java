43e48536178499a7e65ca246388f3426f162de51
WilliamHXu <william.haofei.xu@gmail.com>
2019-04-05 11:16:19 -0400

    added spring boot

---
+++ src/main/java/zipcode/group/walf/WalfApplication.java
@@ -0,0 +1,13 @@
+package zipcode.group.walf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zipcode/group/walf/WalfApplicationTests.java
@@ -0,0 +1,16 @@
+package zipcode.group.walf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WalfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
